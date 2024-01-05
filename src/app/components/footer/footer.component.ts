f82f44ab13c50542eebd628af34aa1eb1adf9464
leirdan <uouleirdan@gmail.com>
2024-01-05 14:09:36 -0300

    :sparkles: feat: título da página no footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+	selector: "app-footer",
+	templateUrl: "./footer.component.html",
+	styleUrls: ["./footer.component.css"],
 })
 export class FooterComponent implements OnInit {
+	pageTitle!: string;
 
-  constructor() { }
+	constructor(private router: Router) {
+		this.router.events.subscribe((e) => {
+			if (e instanceof NavigationEnd) {
+				let aux: string = e.urlAfterRedirects.replace("/", "").replace(/([A-Z])/g, " $1");
 
-  ngOnInit(): void {
-  }
+				this.pageTitle = aux;
+			}
+		});
+	}
 
+	ngOnInit(): void {}
 }
